24fdd4c241ea0c5d9c83ef52d34bc61bca9c5957
Gustavo Gamero <g.gustavogamero@gmail.com>
2025-11-20 20:40:14 -0300

    arreglo en perfil

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -45,16 +45,19 @@ function handleLogUser() {
 
     <div>
       <h3 class="py-1 px-3">Tus perfiles</h3>
-      <ul>
+      <ul class="">
         <li v-for="profile of user.profiles" :key="profile.id">
           <details class="mb-2">
             <summary class="bg-white mx-3 px-2 py-1 rounded-t">
               {{ profile.name }}
             </summary>
 
-            <p class="px-2 mx-3 py-1 rounded-b bg-white/50">
-              {{ profile.ingredients.map(i => i.name).join(', ') }}
-            </p>
+            <ul class="px-2 mx-3 py-2 rounded-b bg-white/50 grid grid-cols-2 gap-x-4 gap-y-1 text-sm">
+              <li v-for="ingredient in profile.ingredients" :key="ingredient.id" class="flex items-start gap-1">
+                <span class="mt-[6px] inline-block h-1.5 w-1.5 rounded-full bg-[#009160]"></span>
+                <span>{{ ingredient.name }}</span>
+              </li>
+            </ul>
           </details>
         </li>
       </ul>
