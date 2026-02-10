9c1015d217cc4217e6474ff44f657e1def4bbaa5
corbalan-nicolas <nicolas.lcorbalan@davinci.edu.ar>
2026-02-10 17:09:30 -0300

    VitePWA plugin

--- src/main.js
+++ src/main.js
@@ -4,4 +4,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router.js'
 
+// Service worker
+import { registerSW } from 'virtual:pwa-register'
+
+const intervalMS = 60 * 60 * 1000
+const updateSW = registerSW({
+    onRegistered(r) {
+        r && setInterval(() => {
+            r.update()
+        }, intervalMS)
+    }
+})
+
 createApp(App).use(router).mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
+import {pwaConfig} from './vite.pwa.config.js'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [
+    vue(),
+    tailwindcss(),
+    VitePWA(pwaConfig)
+  ],
 })
