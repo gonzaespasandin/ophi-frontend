a71815f6fe3a6f893f68a183af5fddf6d488465b
corbalan-nicolas <nicolas.lcorbalan@davinci.edu.ar>
2026-02-13 21:57:52 -0300

    [Inicio] Fix brand name

--- src/components/ui/RecomendedProducts.vue
+++ src/components/ui/RecomendedProducts.vue
@@ -102,7 +102,7 @@ const modules = [A11y, Virtual];
                 >
                     <swiper-slide  v-for="(p, index) of merged" :key="index" :virtual-index="index" v-slot="{ isActive }" class="py-4">
                         <div :class="isActive ? 'min-h-[150px] recomended-card-active' : 'min-h-[100px] mt-2 recomended-card-inactive'" class="flex bg-[#005B8E] p-5 rounded-[11px] flex-col justify-center text-white transition-all ">
-                            <h3 class="text-sm text-center font-regular"><RouterLink :to="'/product/' + p.product.name + '/' + p.product.brand">{{ p.product.name }}</RouterLink></h3>
+                            <h3 class="text-sm text-center font-regular"><RouterLink :to="'/product/' + p.product.name + '/' + p.product.brand.name">{{ p.product.name }}</RouterLink></h3>
                             <div class="bg-[#f5f5f5] text-[#005B8E] p-1 mt-1 rounded-[11px]">
                                 <span class=" text-lg block text-center" :class="p.userName.length > 15 ? 'text-sm': 'text-lg'">{{ p.userName }}</span> 
                                 <p class="text-lg font-bold text-center">RECOMENDADO</p>   
