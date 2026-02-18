5950932d87bb1e7f26600e876dd75585424f040e
Facundo Gamero <gamero.facundo1@gmail.com>
2026-02-18 19:50:31 -0300

    Merge with Gonza | Fixes register validation

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.3tlical14eo"
+    "revision": "0.v9drev3i23"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
